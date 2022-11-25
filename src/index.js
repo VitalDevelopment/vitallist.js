7bba1013c843f96d8cc5596954ed11b86c81ba61
Bryden Calaway <Brydenwcgames@gmail.com>
2022-11-24 22:17:15 -0600

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const baseURL = `https://vitallist.xyz/api`
 
 module.exports = {
     postStats: async (client, apikey) => {
-    const response = await fetch(`https://vitallist.xyz/api/bots/${client.user.id}`, {
+    const response = await fetch(`${baseURL}/bots/${client.user.id}`, {
         method: 'POST',
         headers: {
             'authorization': apikey,
@@ -12,19 +13,19 @@ module.exports = {
         },
       })
       const data = await response.json();
-      console.log(data);
+      console.log(`[VitalList API]: ${data.message}`);
     return;
   },
   fetchBot: async (botid) => {
-    let res = await fetch(`https://vitallist.xyz/api/bots/${botid}`)
+    let res = await fetch(`${baseURL}/bots/${botid}`)
     return await res.json()
   },
   fetchVotes: async (botid) => {
-    let res = await fetch(`https://vitallist.xyz/api/bots/${botid}/votes`)
+    let res = await fetch(`${baseURL}/bots/${botid}/votes`)
     return await res.json()
   },
   checkVote: async (botid, userid) => {
-    let res = await fetch(`https://vitallist.xyz/api/bots/${botid}/voted/?user=${userid}`)
+    let res = await fetch(`${baseURL}/bots/${botid}/voted/?user=${userid}`)
     return await res.json()
   },
 
